18fbbdce099513e3ae871171a50c651e868e9c5b
Ivan5 <converse_ivan@hotmail.com>
2020-05-28 18:12:51 -0500

    Make the initial setup in the backend part

---
+++ backend/data.js
@@ -0,0 +1,48 @@
+export const data = {
+  products: [
+    {
+      _id: "1",
+      name: "Slim Shirt",
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum quasi itaque vitae adipisci ipsam praesentium, vero porro sint cupiditate fugiat, dicta non ducimus voluptate earum nisi voluptatum. Nulla, commodi placeat.",
+      image: "https://via.placeholder.com/150",
+      price: 60,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+    },
+    {
+      _id: "2",
+      name: "Slim Shirt",
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum quasi itaque vitae adipisci ipsam praesentium, vero porro sint cupiditate fugiat, dicta non ducimus voluptate earum nisi voluptatum. Nulla, commodi placeat.",
+      image: "https://via.placeholder.com/150",
+      price: 60,
+      brand: "Adidas",
+      rating: 4.5,
+      numReviews: 10,
+    },
+    {
+      _id: "3",
+      name: "Slim Shirt",
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum quasi itaque vitae adipisci ipsam praesentium, vero porro sint cupiditate fugiat, dicta non ducimus voluptate earum nisi voluptatum. Nulla, commodi placeat.",
+      image: "https://via.placeholder.com/150",
+      price: 60,
+      brand: "Reebok",
+      rating: 4.5,
+      numReviews: 10,
+    },
+    {
+      _id: "4",
+      name: "Slim Shirt",
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum quasi itaque vitae adipisci ipsam praesentium, vero porro sint cupiditate fugiat, dicta non ducimus voluptate earum nisi voluptatum. Nulla, commodi placeat.",
+      image: "https://via.placeholder.com/150",
+      price: 60,
+      brand: "Fila",
+      rating: 4.5,
+      numReviews: 10,
+    },
+  ],
+};
